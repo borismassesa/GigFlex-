713bf95839e1ade28e51394de3f7f63f29f0b057
Boris Massesa <104251801+borismassesa@users.noreply.github.com>
2025-05-12 18:02:41 -0700

    Fix Metro configuration and entry point

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,16 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname);
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
+
+// Add any custom configurations here
+config.resolver = {
+  ...config.resolver,
+  sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx']
+};
 
 module.exports = config;
