f96f9b92fcea9a4f464426a8da6651acbc3ec66c
Boris Massesa <104251801+borismassesa@users.noreply.github.com>
2025-05-12 18:17:43 -0700

    Fix Metro bundler configuration

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,11 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-  // Enable CSS support
-  isCSSEnabled: true,
-});
+const config = getDefaultConfig(__dirname);
 
-// Add any additional configuration overrides here
+// Add the additional resolver options
 config.resolver = {
   ...config.resolver,
   sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs'],
@@ -16,7 +14,7 @@ config.resolver = {
 
 // Ensure watchFolders includes the project root
 config.watchFolders = [
-  path.resolve(__dirname, '.'),
+  path.resolve(__dirname),
   path.resolve(__dirname, 'node_modules'),
 ];
 
